aee334f23b5b510c6bef6810b4a1357fdb89cee5
Pascal Steffen <pascalsteffenbusiness@hotmail.com>
2022-10-17 07:57:04 +0200

    git ftp

---
+++ ftp-push.bat
@@ -0,0 +1,2 @@
+git config git-ftp.syncroot dist/ring-of-fire/
+git ftp push
--- up.bat
+++ up.bat
@@ -2,6 +2,4 @@ git add .
 git commit -m "%*"
 git push
 ng build
-git config git-ftp.syncroot dist/ring-of-fire/
-git ftp push
-
+.\ftp-push.bat
